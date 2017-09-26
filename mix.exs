da9c188e29016d3b35ed47e588d9245d2a49404c
Arnaldo Tayao Jr <arnaldotayaojr@gmail.com>
2017-09-26 14:36:05 +0800

    Adds dependencies

--- mix.exs
+++ mix.exs
@@ -22,8 +22,8 @@ defmodule GooglePlacesApi.Mixfile do
   defp deps do
     [
       {:httpoison, "~> 0.13"},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:poison, "~> 3.1"},
+      {:mock, "~> 0.2.0", only: :test}
     ]
   end
 end
